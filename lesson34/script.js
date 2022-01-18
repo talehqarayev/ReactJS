e783bd4fa08f15e1912440337d9779d84b7692a1
Шукран <47711321+talehqarayev@users.noreply.github.com>
2022-01-18 12:03:36 +0400

    Added lesson34

---
+++ lesson34/script.js
@@ -0,0 +1,36 @@
+window.addEventListener("DOMContentLoaded", () => {
+
+    const box = document.querySelector(".box");
+
+
+    // box.addEventListener("touchstart", (e) => {
+        // e.preventDefault();
+    //     console.log("You touched on the screen of mobile");
+    // });
+
+
+    // box.addEventListener("touchmove", (e) => {
+        // e.preventDefault();
+    //     console.log("You touched and moving on the screen of mobile");
+    // });
+
+
+    // box.addEventListener("touchend", (e) => {
+    //     e.preventDefault();
+    //     console.log("You ended touching on the screen of mobile");
+    // });
+
+
+    box.addEventListener("touchmove", (e) => {
+        e.preventDefault();
+        console.log("You ended touching on the screen of mobile");
+
+        //         // События в объекте
+        // console.log(e.touches); // список всех палцов на экране телефона
+        console.log(e.targetTouches[0].pageX);
+    });
+
+
+});
+
+
