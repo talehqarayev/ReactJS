c0c3afc7398680d6f3829395554deb193b612c1f
Шукран <47711321+talehqarayev@users.noreply.github.com>
2022-02-23 16:09:29 +0400

    Add files via upload

---
+++ lesson7/script.js
@@ -0,0 +1,27 @@
+"use strict";
+
+// alert("Hello World!");
+
+// const answerOfUser = confirm("Вы Гараев Ш.?"); // есть два ответа, да или нет. Результат ответа записывается в переменную answerOfUser
+// console.log(answerOfUser);
+
+
+// const answerOfUser = +prompt("Сколько Вам лет?", ""); // у пользователя есть возможность вести ответ в ручном виде. +prompt === ВСЕГДА числовой тип данных
+// console.log(typeof(answerOfUser)); // Тип данных сторка будет ВСЕГДА
+// console.log(answerOfUser + 5); // когда плюсуем ответ пользователя и любое число тогда ставим плюс перед промпт чтобы был в числовом типе. Иначе число клеится к ответу
+
+
+// const answersOfUser = [];
+
+// answersOfUser[0] = prompt ("What is your name?", "");
+
+// answersOfUser[1] = prompt ("What is your surname?", "");
+
+// answersOfUser[2] = prompt ("How old are you?", "");
+
+// document.write(answersOfUser);
+
+// console.log(typeof(answersOfUser)); // Массив === специальный тип объекта
+
+
+
---
+++ lesson8/script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const category = "Books";
+
+console.log("https://www.somelink.com/" + category + "/" + "4"); // Конкатенация строк. Один кусок строки клеим к другому
+
+    // ИНТЕРПОЛЯЦИЯ
+
+console.log(`https://www.somelink.com/${category}/5`); 
+
+
+const user = "Taleh";
+alert(`Hello, ${user}!`);
