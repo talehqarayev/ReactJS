6f0625726a5c7b9f925879b9d465a08cfb7de553
Taleh <talehqarayev@mail.ru>
2022-03-27 09:32:52 +0400

    added lesson26

---
+++ lesson26/script.js
@@ -0,0 +1,26 @@
+"use strict";
+
+    // МЕНЯТЬ ТИПЫ ДАННЫХ ДИНАМИЧЕСКИ
+
+// TO string
+
+//1
+console.log(typeof(String(4)));
+
+//2 Конкатенация - скалдиваем со строкой любое значение и получаем STRING
+console.log(typeof(5 + ""));
+
+
+// TO number
+
+//1
+console.log(typeof(Number("4")));
+
+//2 - Унарный плюс
+console.log(typeof(+"4"));
+
+
+
+
+// ВСЕ ЧТО ПОЛУЧАЕМ ОТ ПОЛЗОВАТЕЛЯ, ЭТО ТИП ДАННЫХ СТРОКА
+
